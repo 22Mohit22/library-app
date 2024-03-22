5d87bdec8225c3f91084c2bb12566fb2fd64d243
Mohit pandey <mohit20020422@gmail.com>
2024-03-22 19:39:15 +0530

    updated script according to class usage

--- files/script.js
+++ files/script.js
@@ -1,43 +1,3 @@
-let myLibrary = [
-    {
-        id: 0,
-        title: 'Ashtavakra Geeta',
-        author: 'Ashtavakra',
-        pages: 120,
-        read: false
-    },
-    {
-        id: 1,
-        title: 'Bhagwat Geeta',
-        author: 'Krishna Dwaipan',
-        pages: 840,
-        read: true
-    },
-    {
-        id: 2,
-        title: 'Ram Charit Manas ',
-        author: 'Tulisdas',
-        pages: 4560,
-        read: false
-    },
-    {
-        id: 3,
-        title: 'Mahabharat',
-        author: 'Krishna Dwaipan',
-        pages: 15000,
-        read: false
-    },
-];
-
-function Book(title, author, pages, read, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read; // true or false
-    this.id = id;
-}
-
-
 const formEl = document.querySelector('form');
 const titleEl = document.getElementById('book-title');
 const authorEl = document.getElementById('book-author');
@@ -52,19 +12,69 @@ const dialog = document.querySelector('dialog');
 const closeBtn = document.querySelector('#close-btn');
 
 
-
-function check_checkbox() {
-    return true ? checkboxEl.checked : false;
+let library = {
+    mylibrary: [
+        {
+            id: 0,
+            title: 'Ashtavakra Geeta',
+            author: 'Ashtavakra',
+            pages: 120,
+            read: false
+        },
+        {
+            id: 1,
+            title: 'Bhagwat Geeta',
+            author: 'Krishna Dwaipan',
+            pages: 840,
+            read: true
+        },
+        {
+            id: 2,
+            title: 'Ram Charit Manas ',
+            author: 'Tulisdas',
+            pages: 4560,
+            read: false
+        },
+        {
+            id: 3,
+            title: 'Mahabharat',
+            author: 'Krishna Dwaipan',
+            pages: 15000,
+            read: false
+        },
+    ]
 }
 
 
-function createBook() {
-    const book = new Book(titleEl.value, authorEl.value, pagesEl.value, check_checkbox(), myLibrary.length+ 1);
-    return book;
-}
+class Book {
+    constructor(title, author, pages, read, id) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = id;
+    }
 
-function addBookToLibrary() {
-    myLibrary.push(createBook());
+    check_checkBox() {
+        return true ? checkboxEl.checked : false;
+    }
+
+    createBook() {
+        const book = new Book(titleEl.value, authorEl.value, pagesEl.value, mybook.check_checkBox(), library.mylibrary.length+ 1);
+        return book;
+    }
+
+    addBookTolibrary() {
+        library.mylibrary.push(this.createBook());
+    }
+
+    
+
+    appendBookToDisplay() {
+        this.addBookTolibrary();
+        addBookToDisplay(library.mylibrary[library.mylibrary.length - 1]);
+    }
+
 }
 
 function addBookToDisplay(book) {
@@ -100,31 +110,25 @@ function addBookToDisplay(book) {
     nameContainer.appendChild(bookAuthor);
     btnContainer.appendChild(readBtn);
     btnContainer.appendChild(delBtn);
+    
     booksContainer.appendChild(bookCard);
 }
-
-function appendBookToDisplay() {
-    addBookToLibrary();
-    addBookToDisplay(myLibrary[myLibrary.length - 1]);
-}
-
 
 submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     if(titleEl.value.trim() != '' 
     && authorEl.value.trim() != '' 
     && pagesEl.value.trim() != '') {
-        appendBookToDisplay();
+        mybook.appendBookToDisplay();
         titleEl.value = '';
         authorEl.value = '';
         pagesEl.value = '';
         checkboxEl.checked = false;
         dialog.close()
     } else {
-        console.log('please fill all the required elements');
+        alert('please fill all the required elements');
     }
 })
-
 
 addNewBookBtn.addEventListener('click', () => {
     dialog.showModal();
@@ -134,17 +138,11 @@ closeBtn.addEventListener('click', () => {
     dialog.close();
 })
 
-
-
-myLibrary.forEach(book => {
-    addBookToDisplay(book);
-})
-
 booksContainer.addEventListener('click', (e) => {
     const bookCard = e.target.closest('.book-card');
     const displayid = bookCard.getAttribute('data');
-    const indexOfBook = myLibrary.findIndex(book => book.id == displayid);
-    const book = myLibrary[indexOfBook];
+    const indexOfBook = library.mylibrary.findIndex(book => book.id == displayid);
+    const book = library.mylibrary[indexOfBook];
     const readStatus = book.read;
 
     if(e.target.classList.contains('mark-read')) {
@@ -155,13 +153,17 @@ booksContainer.addEventListener('click', (e) => {
         } else {
             e.target.textContent = 'Mark as Read';
         }
-        
+
     } else if(e.target.classList.contains('del-btn')) {
         
         if(indexOfBook != -1) {
-            myLibrary.splice(indexOfBook, 1);
+            library.mylibrary.splice(indexOfBook, 1);
             bookCard.remove();
         }
     }
 })
 
+let mybook = new Book();
+library.mylibrary.forEach(book => {
+    addBookToDisplay(book);
+})
